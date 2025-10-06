e50425dc22a62259d010ed8f44a3b3381a61a577
Júlio Flávio <julioflaviojunior.bh@gmail.com>
2025-10-05 22:26:42 -0300

    Campos inválidos em vermelho

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,13 +3,24 @@ function mudarMenu() {
   menu.hasClass("hidden") ? menu.removeClass("hidden") : menu.addClass("hidden");
 }
 
-addEventListener("submit", function(event) {
-  
-  Swal.fire({
-    title: "Sua mensagem foi recebida com sucesso!",
-    text: "Entraremos em contato em breve.",
-    icon: "success"
-  });
+function enviarFormulario() {
 
-  event.preventDefault();
-})
+  const nome = $("#nomeForm");
+  const email = $("#emailForm");
+  const tel = $("#telForm");
+  const assunto = $("#assuntoForm");
+  const mensagem = $("#mensagemForm");
+  const politica_de_privacidade = $("#politica-privacidade")
+  const inputs = [nome, email, tel, assunto, mensagem]
+
+  inputs.forEach(input => {
+    if (input.val(null)) {
+      input.addClass("border-red-500 outline-red-500")
+      Swal.fire({
+        title: "Erro ao enviar mensagem!",
+        text: `Os campos destacados não foram preenchidos adequadamente`,
+        icon: "error"
+      });
+    }
+  });
+}
