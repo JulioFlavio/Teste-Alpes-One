66044965f12f36924c4f6a59e2d9ee663d3e2639
Júlio Júnior <julioflaviojunior.bh@gmail.com>
2025-10-07 15:17:53 -0300

    Melhorando legibilidade

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -39,39 +39,51 @@ form.addEventListener("submit", function (event) {
     Swal.fire({ title: "Erro!", text: "O nome não pode estar vazio!", icon: "error" });
     $("#nomeForm").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#nomeForm").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#nomeForm").removeClass("border-red-500 outline-red-500")
+  };
 
   const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailValido.test(email)) {
     Swal.fire({ title: "Erro!", text: "O email não foi preenchido adequadamente!", icon: "error" });
     $("#emailForm").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#emailForm").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#emailForm").removeClass("border-red-500 outline-red-500")
+  };
 
   const telefoneLimpo = telefone.replace(/\D/g, "");
   if (telefoneLimpo.length < 10) {
     Swal.fire({ title: "Erro!", text: "O telefone não foi inserido adequadamente.", icon: "error" });
     $("#telForm").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#telForm").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#telForm").removeClass("border-red-500 outline-red-500")
+  };
 
   if (opcao === "") {
     Swal.fire({ title: "Erro!", text: "Selecione uma opção!", icon: "error" });
     $("#assuntoForm").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#assuntoForm").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#assuntoForm").removeClass("border-red-500 outline-red-500")
+  };
 
   if (mensagem === "") {
     Swal.fire({ title: "Erro!", text: "A mensagem não pode estar vazia!", icon: "error" });
     $("#mensagemForm").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#mensagemForm").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#mensagemForm").removeClass("border-red-500 outline-red-500")
+  };
 
   if (!termos) {
     Swal.fire({ title: "Erro!", text: "Você deve aceitar os termos.", icon: "error" });
     $("#politica-privacidade").addClass("border-red-500 outline-red-500");
     return;
-  } else $("#politica-privacidade").removeClass("border-red-500 outline-red-500");
+  } else {
+    $("#politica-privacidade").removeClass("border-red-500 outline-red-500")
+  };
 
   // sucesso
   Swal.fire({
